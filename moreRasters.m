4e29533195745986b8bbd5d4a54668b38b62759c
IvaStevens <ivastevens@gmail.com>
2019-03-20 22:00:25 -0400

    Add files via upload
    first commit

---
+++ moreRasters.m
@@ -0,0 +1,43 @@
+% Load Data
+load('/home/ivana/Desktop/Data/Formatted/Sonic.00073.mat');
+DataF = Data;
+load('/home/ivana/Desktop/Data/Intermediate/Sonic.00073.mat');
+
+% Find where id == 3 (eg: when in forceRamp task state)
+forceTaskState = Data.QL.Data.TASK_STATE_CONFIG.id == 3;
+
+% Find where id+1 == 4 (eg: sucessful forceRamp to Move progression)
+nextTaskState = [diff(Data.QL.Data.TASK_STATE_CONFIG.id), 0] == 1;
+force2move = forceTaskState.*nextTaskState;
+
+% Find the timestamps where the trial succeeded
+nextTaskState = [nextTaskState(3:end), 0, 0];
+force2end = force2move.*nextTaskState;
+
+% Find the timestamps for these events
+successTimes = Data.QL.Data.TASK_STATE_CONFIG.ts_time(force2end==1);
+
+% Sort the timestamps by task requirements
+targets =  Data.QL.Data.TASK_STATE_CONFIG.target(1:4,:);
+reducedTargets = targets(:,force2end==1);
+
+% For each neural unit...
+% Find which units have activity on them
+activeUnits = sum( Data.QL.Data.RAW_SPIKECOUNT.counts,2) ~= 0;
+spikeLocations = activeUnits > 0;
+
+% Find index for timestamp at same time as a successTime
+spikeInd = getSpikeIndices(successTimes,...
+    Data.QL.Data.RAW_SPIKECOUNT.source_timestamp,...
+    0.01);
+
+spikes4plotting = splitSamples(Data.QL.Data.RAW_SPIKECOUNT.counts,...
+    spikeInd,...
+    20); % 200 ms before and after force release
+
+% There are 4 trial types (one force and 4 directions)
+% Find indices for each
+output = separateTrialType(targets, spikes4plotting);
+
+
+
---
+++ separateTrialType.m
@@ -0,0 +1,15 @@
+function output = separateTrialType(targets, formattedSpikes)
+% Split data by trial type.
+% so N {MnxP} matrix where n is the number of trial types
+% Mn is the number of trials of this type
+% P is the length of the rastervalues.
+% Returns an 1xN cell of (Mn x P) matrices.
+
+[~, ~, types] = unique( targets', 'rows');
+nTypes = max(types);
+output = cell(1,nTypes);
+
+for mi = 1 : nTypes
+    indices = types == mi;
+    output{mi} = formattedSpikes(indices,:);
+end
